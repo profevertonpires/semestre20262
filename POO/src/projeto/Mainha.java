613e2e9ed4343d5ff074540e66d844eb9e9d4afc
profevertonpires <everton.pires@pro.ucsal.br>
2026-08-15 09:17:42 -0300

    Primeira aula
    Primeira aula de POO

---
+++ POO/src/projeto/Mainha.java
@@ -0,0 +1,13 @@
+package projeto;
+
+public class Mainha {
+	public static void main(String[] args) {
+		
+		Pessoa pessoa = new Pessoa();
+		System.out.println(pessoa);
+		
+		pessoa = new Pessoa();
+		System.out.println(pessoa);
+	}	
+
+}
